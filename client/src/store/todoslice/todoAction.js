aa231b65a97b84dbd15528f18ab33dc743db8d52
unknown <you@example.com>
2024-02-10 12:28:03 +0530

    Update

--- client/src/store/todoslice/todoAction.js
+++ client/src/store/todoslice/todoAction.js
@@ -3,12 +3,12 @@ import axios from "axios";
 
 export const fetchTodos = (page, limit) => async (dispatch) => {
   try {
-    const response = await axios.get(
-      `http://localhost:5000/todos?page=${page}&limit=${limit}`
-    );
     // const response = await axios.get(
-    //   `https://todo-list-api-five.vercel.app/todos?page=${page}&limit=${limit}`
+    //   `http://localhost:5000/todos?page=${page}&limit=${limit}`
     // );
+    const response = await axios.get(
+      `https://todo-list-api-five.vercel.app/todos?page=${page}&limit=${limit}`
+    );
     dispatch(allTodos(response.data));
   } catch (error) {
     console.error(error);
